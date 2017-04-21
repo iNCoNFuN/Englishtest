ac00113569c305f9a69d7b89361f92af8d6100b1
Manuel <inconfunsion@gmail.com>
2017-04-21 19:35:06 +0200

    First changes to add menu open and close windows

--- index.js
+++ index.js
@@ -1,36 +1,69 @@
 var electron = require('electron');
 var BrowserWindow = electron.BrowserWindow;
 var app = electron.app;
+var ipc = electron.ipcMain;
 
 // Event for when electron is ready
 app.on('ready', function() {
 
-  var appWindow, acercadeWindow;
+  var appWindow, questionsWindow, acercadeWindow;
   appWindow = new BrowserWindow({
     show: false
   });
-
   // Load up the main window
   appWindow.loadURL('file://' + __dirname + '/index.html');
+
+
+  // Load up the main window
+  questionsWindow = new BrowserWindow({
+    show: false
+  });
+  questionsWindow.loadURL('file://' + __dirname + '/questions.html');
+
   acercadeWindow = new BrowserWindow({
     width: 384,
     height: 232,
-    show: false
+    show: false,
+    transparent: true,
+    frame: false
   });
-
   // Load acercade.html file up
   acercadeWindow.loadURL('file://' + __dirname + '/acercade.html');
+
 
 
   // Do the magic when the app is ready to show
   appWindow.once('ready-to-show', function() {
     appWindow.show(); //Show the main window
-    setTimeout(function() {
-      acercadeWindow.show(); //Show it up after one second
-      setTimeout(function() {
-        acercadeWindow.hide(); //Hide it up after three
-      }, 3000);
-    }, 1000);
+    //questionsWindow.show(); //Show the questions window
+    // setTimeout(function() {
+    //   acercadeWindow.show(); //Show it up after one second
+    //   // setTimeout(function() {
+    //   //   acercadeWindow.hide(); //Hide it up after three
+    //   // }, 3000);
+    // }, 1000);
   });
+
+//Abrir acercadeWindow
+ipc.on('abrirAcercadeWindow', function(event, arg){
+  event.returnValue=''; //Turn back an empty value
+  acercadeWindow.show();
+});
+//Close acercadeWindow
+ipc.on('closeAcercadeWindow', function(event, arg){
+  event.returnValue=''; //Turn back an empty value
+  acercadeWindow.hide();
+});
+
+//Abrir questionsWindow
+ipc.on('abrirQuestionsWindow', function(event, arg){
+  event.returnValue=''; //Turn back an empty value
+  questionsWindow.show();
+});
+//Close questionsWindow
+ipc.on('closeQuestionsWindow', function(event, arg){
+  event.returnValue=''; //Turn back an empty value
+  questionsWindow.hide();
+});
 
 });
---
+++ tests/test_000001.js
@@ -0,0 +1,23 @@
+const test_000001 = {
+  {
+    name: 'Test 000001',
+    description: 'Test description'
+  },
+  {
+    question0001: {
+      name: 'Adverbs with <strong>TO Be</strong>',
+      description: 'Description 0001',
+      question: 'Where will the adverb go if the sentence is affirmative or negative?',
+      answers: {
+        a1: 'Before the to be verb "I always am happy"',
+        a2: 'Behind the to be verb "I am always happy"',
+        a3: 'At the end of the phrase "I am happy always"'
+      },
+      solution: {
+        answer: '2',
+        solution: 'The adverbs with <strong>to be</strong> verb always go after the verb.'
+      }
+
+    }
+  }
+};
